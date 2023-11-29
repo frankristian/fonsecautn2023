aba4988c2c82913ae278da3a14f0aa7913ce6310
Cristian Fonseca <cristian@fonseca.com>
2023-11-28 23:08:36 -0300

    Modulo 6 Unidad 4 - Se agrega funcionalidad de envío de email

--- transportes/backend/routes/api.js
+++ transportes/backend/routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 var novedadesModel = require('./../models/novedadesModel');
 
 router.get('/novedades', async function(req, res, next) {
@@ -28,5 +29,31 @@ router.get('/novedades', async function(req, res, next) {
 
     res.json(novedades);
 });
+
+// Handler de contacto y envío de mail
+router.post('/contacto', async(req, res) =>{
+    const mail = {
+        to:'transportesX@gmail.com',
+        subject: 'contacto web sitio Transporte',
+        html: `${req.body.nombre} se contactó a traves de la web y quiere más información a este correo ${req.body.email}
+        <br> Además, hizo el siguiente comentario: ${req.body.mensaje} <br> Su tel es: ${req.body.telefono}`
+    }
+
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+
+    await transport.sendMail(mail);
+    res.status(201).json({
+        error: false,
+        message: 'Mensaje enviado'
+    })
+}); // Fin envío de mail.
+
 
 module.exports = router;
--- transportes/frontend/src/pages/Contacto/ContactoPage.js
+++ transportes/frontend/src/pages/Contacto/ContactoPage.js
@@ -1,43 +1,76 @@
-import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 import './ContactoPage.css';
 
 const ContactoPage = (props) => {
+
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        mensaje: ''
+    };
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value
+        }));
+    }
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true);
+        const response = await axios.post('http://localhost:4200/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialForm);
+        }
+    }
+
     return (
         <main class="holder contacto">
-        <div>
-            <h2>Contacto Rápido</h2>
-            <form action="" method="" class="formulario">
-                <p><label for="nombre">Nombre</label>
-                    <input type="text" name="nombre" id="nombre"/>
-                </p>
-                <p>
-                    <label for="email">Email</label>
-                    <input type="text" name="email" id="email"/>
-                </p>
-                <p>
-                    <label for="telefono">Teléfono</label>
-                    <input type="text" name="telefono" id="telefono"/>
-                </p>
-                <p><label for="mensaje">Mensaje</label>
-                    <textarea name="mensaje" id="mensaje" cols="30" rows="10"></textarea>
-                </p>
-                <p className='acciones'>
-                    <input type="submit" value="Enviar"/>
-                </p>
-            </form>
-        </div>
-        <div className="datos">
-            <h2>Otras vías de comunicación</h2>
-            <p>También puede contactarse con nosotros usando los siguientes medios</p>
-            <ul>
-                <li>Teléfono: 43242388</li>
-                <li>Email: contacto@transportesx.com.ar</li>
-                <li>Facebook: transportesX</li>
-                <li>Twitter: @transportesX</li>
-                <li>skype: transportes_x</li>
-            </ul>
-        </div>
-    </main>
+            <div>
+                <h2>Contacto Rápido</h2>
+                <form action="/contacto" method="" class="formulario" onSubmit={handleSubmit}>
+                    <p><label for="nombre">Nombre</label>
+                        <input type="text" name="nombre" id="nombre" value={formData.nombre} onChange={handleChange}/>
+                    </p>
+                    <p>
+                        <label for="email">Email</label>
+                        <input type="text" name="email" id="email" value={formData.email} onChange={handleChange}/>
+                    </p>
+                    <p>
+                        <label for="telefono">Teléfono</label>
+                        <input type="text" name="telefono" id="telefono" value={formData.telefono} onChange={handleChange}/>
+                    </p>
+                    <p><label for="mensaje">Mensaje</label>
+                        <textarea name="mensaje" id="mensaje" cols="30" rows="10" value={formData.mensaje} onChange={handleChange}></textarea>
+                    </p>
+                    <p className='acciones'>
+                        <input type="submit" value="Enviar" />
+                    </p>
+                    {sending ? <p>Enviando...</p> : null}
+                    {msg ? <p>{msg}</p>: null}
+                </form>
+            </div>
+            <div className="datos">
+                <h2>Otras vías de comunicación</h2>
+                <p>También puede contactarse con nosotros usando los siguientes medios</p>
+                <ul>
+                    <li>Teléfono: 43242388</li>
+                    <li>Email: contacto@transportesx.com.ar</li>
+                    <li>Facebook: transportesX</li>
+                    <li>Twitter: @transportesX</li>
+                    <li>skype: transportes_x</li>
+                </ul>
+            </div>
+        </main>
     );
 }
 
